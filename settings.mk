f65c7601e5884161751e48d4ed67869fa9f12d0f
Rashad M <mohammedrashadkm@gmail.com>
2014-10-28 08:54:17 +0530

    Update settings.mk

--- settings.mk
+++ settings.mk
@@ -9,8 +9,6 @@ JOBS := 4
 #MXE_TARGETS :=  i686-pc-mingw32.static i686-pc-mingw32.shared  x86_64-w64-mingw32.static x86_64-w64-mingw32.shared  i686-w64-mingw32.static i686-w64-mingw32.shared
 
 MXE_TARGETS := x86_64-w64-mingw32.shared i686-w64-mingw32.shared
-#MXE_TARGETS := i686-w64-mingw32.shared
-#MXE_TARGETS := i686-w64-mingw32.shared x86_64-w64-mingw32.shared
 
 # This variable controls the download mirror for SourceForge,
 # when it is used. Enabling the value below means auto.
@@ -18,6 +16,6 @@ MXE_TARGETS := x86_64-w64-mingw32.shared i686-w64-mingw32.shared
 
 # The three lines below makes `make` build these "local
 # packages" instead of all packages.
-#LOCAL_PKG_LIST := boost curl file flac lzo pthreads vorbis wxwidgets
-#.DEFAULT local-pkg-list:
-#local-pkg-list: $(LOCAL_PKG_LIST)
+LOCAL_PKG_LIST := gdal
+.DEFAULT local-pkg-list:
+local-pkg-list: $(LOCAL_PKG_LIST)
