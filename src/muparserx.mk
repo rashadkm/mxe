bd0eb1f35c2aeea9fde84cddd2f01dad7ef8848a
Rashad M <mohammedrashadkm@gmail.com>
2015-03-13 17:11:44 +0100

    test sourceforget url for muparserx

--- src/muparserx.mk
+++ src/muparserx.mk
@@ -7,7 +7,7 @@ $(PKG)_VERSION  := v3_0_3
 $(PKG)_CHECKSUM := d476899a024b6b720591484f615464dc1eb25b23
 $(PKG)_SUBDIR   :=
 $(PKG)_FILE     := $(PKG)_v3_0_2.zip
-$(PKG)_URL      := http://muparserx.googlecode.com/svn/archives/$($(PKG)_FILE)
+$(PKG)_URL      := https://sourceforge.net/projects/mxedeps/files/$($(PKG)_FILE)
 $(PKG)_DEPS     := gcc
 
 define $(PKG)_UPDATE
